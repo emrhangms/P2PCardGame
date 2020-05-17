fb26968bc666f9877ef11588dd8fe92a7c975d68
Serhad Eymen ŞENCAN <serhad.sencan@std.izu.edu.tr>
2020-05-17 17:45:38 +0300

    Api temelleri atıldı cümlemize hayırlı uğurlu olsun

--- Assets/_Code/Scripts/Canvas/LoginPanel.cs
+++ Assets/_Code/Scripts/Canvas/LoginPanel.cs
@@ -3,10 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-
+using UnityEngine.Networking;
+using SimpleJSON;
+using TMPro;
+using UnityEngine.UI;
 public class LoginPanel : MonoBehaviour
 {
     public List<CanvasGroup> panels = new List<CanvasGroup>();
+    public InputField userNameLogin;
+    public InputField userNamePassword;
+    public InputField signUserName;
+    public InputField signPassword;
+    public InputField signEmail;
+
 
     void Start()
     {
@@ -35,18 +44,72 @@ public class LoginPanel : MonoBehaviour
     public void Login()
     {
         //TODO GİRİŞ İŞLEMLERİ
+        Debug.Log("girdi");
+        StartCoroutine(GetApi(userNameLogin.text,userNamePassword.text));
         CanvasManager.ins.OpenPanel(PanelNames.PlayerPanel);
     }
 
-    public void CreateAccount()
+    public void OpenCreateAccountPanel()
     {
         //TODO PLAYER YARATMA İŞLEMLERİ
         OpenPanel(1);
     }
-
+    public void CreateAccount()
+    {
+        //TODO PLAYER YARATMA İŞLEMLERİ
+        StartCoroutine(PostApi());
+        OpenPanel(0);
+    }
     void Update()
     {
 
     }
+    
+
+    // Api functions 
+    IEnumerator GetApi(string uname , string password)
+    {
+        Debug.Log("Login");
+        string Url = $"http://134.122.95.112:3005/users/name={uname}/password={password}";
+        Uri uri = new Uri(Url);
+        UnityWebRequest request = UnityWebRequest.Get(uri);
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+            yield break;
+        }
+        JSONNode info = JSON.Parse(request.downloadHandler.text);
+        
+
+        string name = info["name"];
+        Debug.Log("name : " + name);
+    }
+
+    IEnumerator PostApi()
+    {
+        Debug.Log("başladık");
+        string Url = "http://134.122.95.112:3005/users";
+        WWWForm form = new WWWForm();
+        form.AddField("name", signUserName.text);
+        form.AddField("email", signEmail.text);
+        form.AddField("password", signPassword.text);
+
+
+        UnityWebRequest postRequest = UnityWebRequest.Post(Url, form);
+        yield return postRequest.SendWebRequest();
+        if (postRequest.isNetworkError || postRequest.isHttpError)
+        {
+            Debug.Log(postRequest.error);
+            yield break;
+        }
+        else
+        {
+            Debug.Log("başarılı");
+        }
+
+    }
+
+
 
 }
